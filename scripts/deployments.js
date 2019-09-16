4049951891c817819910cb31271a7978e6cde7d4
Alex <git@area.io>
2019-09-16 12:15:24 +0100

    Add !staging and !production deployments

--- scripts/deployments.js
+++ scripts/deployments.js
@@ -3,7 +3,7 @@
 const exec = require('await-exec')
 
 module.exports = async function(robot) {
-  const deployRegex = /!qa deploy ([0-9a-fA-f]*)/
+  const deployRegex = /!(qa|staging|production) deploy ([0-9a-fA-f]*)/
 
   // Activate credentials
   await exec(`echo ${process.env.GCLOUD_SERVICE_KEY} | base64 --decode > /gcloud-service-key.json`);
@@ -13,10 +13,33 @@ module.exports = async function(robot) {
   await exec(`gcloud config set compute/zone ${process.env.GCLOUD_CLOUDSDK_COMPUTE_ZONE}`);
   await exec(`gcloud --quiet container clusters get-credentials ${process.env.GCLOUD_CLUSTER_NAME}`);
 
+  // Get the devops topic ID. The topic will be used to determine what colour is staging / production when asked to deploy to either.
+  let channels = await robot.adapter.client.web.conversations.list();
+  let devopsChannel = channels.channels.filter(channel => channel.name === "devops")[0];
+  const devopsid = devopsChannel.id
+
   robot.hear(deployRegex, async msg => {
     const matches = deployRegex.exec(msg.message.text);
+    if (matches[1] === 'qa'){
+      await exec('kubectl patch deployment dapp-red -p \'{"spec":{"template":{"spec":{"containers":[{"name":"dapp","image":"eu.gcr.io/fluent-aileron-128715/dapp-goerli:' + matches[2] + '"}]}}}}\'')
+    } else if (matches[1] === 'staging' || matches[1] === 'production') {
+      // Get colours
+      const response = await robot.adapter.client.web.channels.info(devopsid);
+      devopsChannel = response.channel;
+      const devopsTopic = devopsChannel.topic.value;
+      let colour;
 
-    await exec('kubectl patch deployment dapp-red -p \'{"spec":{"template":{"spec":{"containers":[{"name":"dapp","image":"eu.gcr.io/fluent-aileron-128715/dapp-goerli:' + matches[1] + '"}]}}}}\'')
+      if (matches[1] === 'staging'){
+        const stagingColourRegex = /Currently staging: ([a-zA-Z]*)/
+        const stagingColourMatches = stagingColourRegex.exec(devopsTopic);
+        colour = stagingColourMatches[1].toLowerCase();
+      } else {
+        const productionColourRegex = /Currently production\/live: ([a-zA-Z]*)/
+        const productionColourMatches = productionColourRegex.exec(devopsTopic);
+        colour = productionColourMatches[1].toLowerCase();
+      }
+      await exec('kubectl patch deployment dapp-' + colour +' -p \'{"spec":{"template":{"spec":{"containers":[{"name":"dapp","image":"eu.gcr.io/fluent-aileron-128715/dapp-main:' + matches[2] + '"}]}}}}\'' )
+    }
     msg.send('Container patched. Could take up to two minutes to start responding.')
   })
 }
