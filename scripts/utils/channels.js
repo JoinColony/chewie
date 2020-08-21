4b851cc84a43daf31bfe27dfeaff92519f8e61c4
area <git@area.io>
2020-08-21 13:44:55 +0100

    Fix issue with telling if Discord message was a DM

--- scripts/utils/channels.js
+++ scripts/utils/channels.js
@@ -1,12 +1,12 @@
 const isPrivateSlackMessage = res => res.message.room.startsWith('D');
 const isPrivateDiscordMessage = (client, res) => {
-  console.log(res);
 	// Get user
 	const user = client.users.get(res.message.user.id);
 	// Get their DM channel with the bot
 	const channel = user.dmChannel
 	// Is that where we saw the message?
-	return channel.id === res.message.room;
+  // If channel is not set, then it definitely wasn't in DM, as we don't have a DM channel open with that user!
+	return !channel || channel.id === res.message.room;
 }
 
 const isChannel = (res, channelId) => res.message.room === channelId;
