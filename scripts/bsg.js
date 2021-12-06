6a1da014e6f8e8a41e8b887ee993d252138441a0
Alex Rea <git@area.io>
2021-12-06 10:00:52 +0000

    Switch hubot provider to one with newer discord.js

--- scripts/bsg.js
+++ scripts/bsg.js
@@ -1,8 +1,8 @@
 const { isChannel, isPrivateDiscordMessage } = require('./utils/channels');
 
 module.exports = function(robot) {
-  robot.hear(/@Chewie, what do you hear\?/, async (res) => {
+  robot.hear(/<@!720953382789185548>, what do you hear\?/, async (res) => {
   	if (res.message.user.id !='273398423787536384') return
-  	res.send('Nothing but the rain.');
+  	res.robot.adapter.send(res.envelope, 'Nothing but the rain.');
   })
 }
